8c48a42d4ec028f29298069b0f29fe66204a0de4
sacbem <sacbem7801@gmail.com>
2022-03-10 18:45:58 +0800

    HW_A

---
+++ GitTutorial.cpp
@@ -0,0 +1,16 @@
+#include<string>
+#include<iostream>
+
+bool    Guess(int number){
+    return false;
+}
+
+int main(){
+
+    int guess ; 
+    do{
+        std::cin>>guess;
+    }while (!Guess(guess));
+
+    return 0;
+}
